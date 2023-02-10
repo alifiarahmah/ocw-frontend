69197a3abe51ebbe5602ded020cf26e2f45bdc40
alifiarahmah <13520122@std.stei.itb.ac.id>
2023-02-11 06:34:09 +0700

    feat: add mobile drawer  to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,45 +1,80 @@
 import {
   Avatar,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerOverlay,
   Heading,
   HStack,
+  IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Text,
+  useDisclosure,
 } from '@chakra-ui/react'
-import { MdOutlineArrowDropDown } from 'react-icons/md'
+import { MdMenu, MdOutlineArrowDropDown } from 'react-icons/md'
 
 export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <HStack
-      as="nav"
-      py={5}
-      px={3}
-      justifyContent="space-between"
-      bg="biru.600"
-      color="white"
-      boxShadow={{ base: 'none', md: 'md' }}
-    >
-      <HStack>
-        <Heading as="h6" size="md">
-          ITBOpenCourseWare
-        </Heading>
+    <>
+      <HStack
+        as="nav"
+        py={5}
+        px={3}
+        justifyContent="space-between"
+        bg="biru.600"
+        color="white"
+        boxShadow={{ base: 'none', md: 'md' }}
+      >
+        <HStack>
+          <Heading as="h6" size="md">
+            ITBOpenCourseWare
+          </Heading>
+        </HStack>
+        <IconButton
+          variant="unstyled"
+          aria-label="Menu"
+          icon={<MdMenu size="2rem" />}
+          display={{ base: 'block', md: 'none' }}
+          onClick={onOpen}
+        />
+        <HStack display={{ base: 'none', md: 'flex' }}>
+          <Menu>
+            <MenuButton>
+              <HStack>
+                <Avatar size="sm" />
+                <Text>Admin</Text>
+                <MdOutlineArrowDropDown />
+              </HStack>
+            </MenuButton>
+            <MenuList color="black">
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </HStack>
       </HStack>
-      <HStack>
-        <Menu>
-          <MenuButton>
-            <HStack>
-              <Avatar size="sm" />
-              <Text>Admin</Text>
-              <MdOutlineArrowDropDown />
-            </HStack>
-          </MenuButton>
-          <MenuList color="black">
-            <MenuItem>Logout</MenuItem>
-          </MenuList>
-        </Menu>
-      </HStack>
-    </HStack>
+
+      {/* Drawer */}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          <DrawerBody mt={7}>
+            <Heading as="h6" size="md">
+              ITBOpenCourseWare
+            </Heading>
+          </DrawerBody>
+
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
