75add1180a814766c36d5425457c88b6f516a46d
alifiarahmah <13520122@std.stei.itb.ac.id>
2023-02-11 07:08:16 +0700

    feat(layout): add box inheritance to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,24 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, BoxProps, Flex } from '@chakra-ui/react'
 import Navbar from './navbar'
 
-export interface LayoutProps {
+export interface LayoutProps extends BoxProps {
   bg?: string
   children: React.ReactNode
 }
+// with extends BoxProps, we can pass any props that Box can accept
+// example: <Layout p={10}>
 
-const Layout: React.FC<LayoutProps> = ({ bg, children }) => {
+const Layout: React.FC<LayoutProps> = ({ bg, children, ...props }) => {
   return (
     <Flex direction="column" minH="100vh" bg={bg ?? 'biru.100'}>
       <Navbar />
-      <Box p={3}>{children}</Box>
+      <Box
+        px={props.p ?? props.px ?? { base: 5, md: 20 }}
+        py={props.p ?? props.py ?? { base: 5, md: 10 }}
+        {...props}
+      >
+        {children}
+      </Box>
     </Flex>
   )
 }
