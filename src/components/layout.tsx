a0a905907aec39454c7e9743401bef3e20b5304a
alifiarahmah <13520122@std.stei.itb.ac.id>
2023-02-13 15:57:49 +0700

    feat(layout): add title for SEO

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,25 +1,33 @@
 import { Box, BoxProps, Flex } from '@chakra-ui/react'
+import Head from 'next/head'
 import Navbar from './navbar'
 
 export interface LayoutProps extends BoxProps {
   bg?: string
+  title?: string
   children: React.ReactNode
 }
 // with extends BoxProps, we can pass any props that Box can accept
 // example: <Layout p={10}>
 
-const Layout: React.FC<LayoutProps> = ({ bg, children, ...props }) => {
+const Layout: React.FC<LayoutProps> = ({ bg, children, title, ...props }) => {
   return (
-    <Flex direction="column" minH="100vh" bg={bg ?? 'biru.100'}>
-      <Navbar />
-      <Box
-        px={props.p ?? props.px ?? { base: 5, md: 20 }}
-        py={props.p ?? props.py ?? { base: 5, md: 10 }}
-        {...props}
-      >
-        {children}
-      </Box>
-    </Flex>
+    <>
+      <Head>
+        <title>{title ? `${title} |` : null} ITBOpenCourseWare</title>
+        <meta name="description" content="Platform pembelajaran kuliah untuk publik yang disediakan oleh Institut Teknologi Bandung." />
+      </Head>
+      <Flex direction="column" minH="100vh" bg={bg ?? 'biru.100'}>
+        <Navbar />
+        <Box
+          px={props.p ?? props.px ?? { base: 5, md: 20 }}
+          py={props.p ?? props.py ?? { base: 5, md: 10 }}
+          {...props}
+        >
+          {children}
+        </Box>
+      </Flex>
+    </>
   )
 }
 
