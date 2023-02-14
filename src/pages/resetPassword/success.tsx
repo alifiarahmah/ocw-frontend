27ddacf092e3a4bb2df2a0767fe1354eee95b6de
Nathanael Santoso <nart4hire@gmail.com>
2023-02-14 07:23:08 +0700

    feat(reset): more skeleton pages

---
+++ src/pages/resetPassword/success.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/layout'
+
+
+export default function ResetPassword() {
+    return (
+        <>
+            <Layout
+            px={{ base: 5, md: 20 }}
+            py={{ base: 5, md: 10 }}
+            title="Reset your Password"
+            >
+            </Layout>
+        </>
+    )
+}
