702f78ee1c7c3062067052a28d591bf95859a3e6
alifiarahmah <13520122@std.stei.itb.ac.id>
2023-02-23 18:34:17 +0700

    feat: add ITB logo to navbar and favicon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,44 +9,53 @@ import {
   Heading,
   HStack,
   IconButton,
+  Image,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Text,
   useDisclosure,
-} from '@chakra-ui/react'
-import { MdMenu, MdOutlineArrowDropDown } from 'react-icons/md'
-import { useRouter } from 'next/router'
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { MdMenu, MdOutlineArrowDropDown } from "react-icons/md";
 
 export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const router = useRouter()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
 
   return (
     <>
       <HStack
         as="nav"
         py={5}
-        px={3}
+        px={7}
         justifyContent="space-between"
         bg="biru.600"
         color="white"
-        boxShadow={{ base: 'none', md: 'md' }}
+        boxShadow={{ base: "none", md: "md" }}
       >
-        <HStack>
-          <Heading as="h6" size="md">
-            ITBOpenCourseWare
-          </Heading>
-        </HStack>
+        <Link href="/">
+          <HStack gap={2}>
+            <Image
+              alt="logo"
+              src="https://seeklogo.com/images/I/institut-teknologi-bandung-logo-7B8F816823-seeklogo.com.png"
+              height="2.5rem"
+            />
+            <Heading as="h6" size="md">
+              ITBOpenCourseWare
+            </Heading>
+          </HStack>
+        </Link>
         <IconButton
           variant="unstyled"
           aria-label="Menu"
           icon={<MdMenu size="2rem" />}
-          display={{ base: 'block', md: 'none' }}
+          display={{ base: "block", md: "none" }}
           onClick={onOpen}
         />
-        <HStack display={{ base: 'none', md: 'flex' }}>
+        <HStack display={{ base: "none", md: "flex" }}>
           <Menu>
             <MenuButton>
               <HStack>
@@ -56,7 +65,7 @@ export default function Navbar() {
               </HStack>
             </MenuButton>
             <MenuList color="black">
-              <MenuItem onClick={() => router.push('/login')}>Logout</MenuItem>
+              <MenuItem onClick={() => router.push("/login")}>Logout</MenuItem>
             </MenuList>
           </Menu>
         </HStack>
@@ -78,5 +87,5 @@ export default function Navbar() {
         </DrawerContent>
       </Drawer>
     </>
-  )
+  );
 }
