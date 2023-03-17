6a79f17c45c2e90f2dda4c0085ee64d832b9e6b0
Muhammad Fahmi Irfan <13520152@std.stei.itb.ac.id>
2023-03-17 20:47:39 +0700

    feat : complete sidebar

--- src/pages/management/course/[course-id]/content/[content-id]/file.tsx
+++ src/pages/management/course/[course-id]/content/[content-id]/file.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import CourseBanner from "@/components/course_banner";
 import {
   Button,
   Card,
@@ -13,65 +14,13 @@ import {
   Stack,
   Container,
   Center,
-  Flex,
-  Box,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerHeader,
-  DrawerBody,
-  Text,
-  BoxProps,
-  Spacer,
-  HStack,
-  VStack,
-  Divider,
 } from "@chakra-ui/react";
 import { MdArrowBackIos } from "react-icons/md";
-
-export interface BannerProps extends BoxProps {
-  bg?: string
-  children: React.ReactNode
-}
-
-const CourseBanner : React.FC<BannerProps> = ({bg, children, ...props}) => {
-  return (
-    <HStack alignItems={"flex-start"} height="100%">
-      <Box
-          mt={0}
-          px={props.p ?? props.px ?? { base: 5, md: 20 }}
-          py={props.p ?? props.py ?? { base: 5, md: 10 }}
-          width='100%'
-          {...props}
-          >
-          {children}
-        </Box>
-      <Container minHeight="90vh" height="inherit" bgColor="magenta" width="50vh" flexGrow={1} overflowY={"auto"} position="absolute">
-        <VStack align={'start'} spacing='6' marginStart='2vh' maxWidth="30vh">
-          <VStack spacing='1' align={'start'}>
-              <Text align={'start'} fontSize={'m'}>Kode Mata Kuliah</Text>
-              <Text align={'start'} fontSize={'xl'} wordBreak="break-word">IF2240</Text>
-          </VStack>
-          <Divider borderColor={'black'} borderWidth={"1px"} w='75%' my={4} style={{marginLeft: "0vh"}}/>
-          <VStack spacing='1' align={'start'}>
-              <Text align={'start'} fontSize={'m'}>Mata Kuliah</Text>
-              <Text align={'start'} fontSize={'xl'} wordBreak="break-word">Pembelajaran Mesin</Text>
-          </VStack>
-          <Divider borderColor={'black'} borderWidth={"1px"} w='75%' my={4} style={{marginLeft: "0vh"}}/>
-          <VStack spacing='1' align={'start'}>
-              <Text align={'start'} fontSize={'m'}>Dosen Pengajar</Text>
-              <Text align={'start'} fontSize={'xl'} wordBreak="break-word">Dr. Nur Ulva Maulidevi, ST, M.Sc</Text>
-          </VStack>
-        </VStack>
-      </Container>
-    </HStack>
-  )
-}
 
 export default function EditContent() {
   return (
     <Layout title="Edit Content" py={0} px={0}>
-      <CourseBanner>
+      <CourseBanner course_code="IF3000" course_name="Pembeleajarajran amesin" lecturer="Bu ulfa">
         <Stack>
           <Stack
             justifyContent="space-between"
@@ -88,34 +37,6 @@ export default function EditContent() {
               </Heading>
             </Stack>
           </Stack>
-          <Card mt={10}>
-            <Center>
-            <CardHeader>
-              <Heading size="md" className="center">Menambah Handout Materi</Heading>
-            </CardHeader>
-            </Center>
-            <CardBody>
-              <form>
-                <FormControl isRequired>
-                  <FormLabel>Judul Handout</FormLabel>
-                  <Input/>
-                </FormControl>
-                <FormControl isRequired>
-                  <FormLabel>Downloadable</FormLabel>
-                  <Select>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                  </Select>
-                </FormControl>
-                <FormControl>
-                  <FormLabel>Upload File</FormLabel>
-                  <Input type="file"></Input>
-                </FormControl>
-                <Container height={30}/>
-                <Button type="submit" backgroundColor="blue.400">Tambah</Button>
-              </form>
-            </CardBody>
-          </Card>
           <Card mt={10}>
             <Center>
             <CardHeader>
