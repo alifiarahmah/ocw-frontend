b6a13dae75ddbd3000647a9d6b640f39cb1935ad
alfatihs <azmi.alfatihs@gmail.com>
2023-03-28 21:47:45 +0700

    fix sidebar using course_banner component

--- src/pages/content_video.tsx
+++ src/pages/content_video.tsx
@@ -1,23 +1,47 @@
-import styles from '@/styles/verifcation.module.css'
-import {Container, Text, HStack, Button} from '@chakra-ui/react'
-import Layout from '@/components/layout'
-import RightSidebar from '@/components/right-sidebar'
+import styles from '@/styles/verifcation.module.css';
+import { Container, Text, HStack, Button } from '@chakra-ui/react';
+import Layout from '@/components/layout';
+import { useState } from 'react';
+import CourseBanner from '@/components/course_banner';
+import { Center } from '@chakra-ui/react';
 
-export default function contentVideo(){
-    return(
-        <Layout title ="Content Layout">
-            <RightSidebar kodeMataKuliah={'IF2240'} mataKuliah={'Pembelajaran Mesin'} dosenPengajar={'Dr. Nur Ulva Maulidevi, ST, M.Sc'}/>            
-            <HStack spacing={'300px'}>
-                <Button _hover={{color: "darkgrey", opacity:'100%'}}    background={'transparent'} fontSize={'6xl'} left={'27vh'}> ← </Button>
-                <Text fontSize={'3xl'} >Decision Tree Learning (DTL)</Text>
-            </HStack> 
-            <Container marginStart={'27vh'}  maxWidth={'5xl'} maxHeight={'7xl'} className = {styles.containercontent}>
+export default function contentVideo() {
+  const [courseBannerProps, setCourseBannerProps] = useState({
+    course_code: 'IF3270',
+    course_name: 'Pembelajaran Mesin',
+    lecturer: 'Dr. Nur Ulfa Maulidevi, ST, M.Sc.',
+  });
+  return (
+    <Layout title="Content Layout" py={0} px={0}>
+      <CourseBanner {...courseBannerProps}>
+        
+          <HStack spacing={'30px'}>
+            <Button
+              _hover={{ color: 'darkgrey', opacity: '100%' }}
+              background={'transparent'}
+              fontSize={'6xl'}
+            >
+            ←</Button>
+            <Text fontSize={'3xl'}>Decision Tree Learning (DTL)</Text>
+          </HStack>
+        <Center flexDirection={'column'}>
+
+          <Container
+            // marginStart={'10vw'}
+            maxWidth={'5xl'}
+            maxHeight={'7xl'}
+            className={styles.containercontent}
+          >
             <iframe 
             width="100%" height="100%" 
-            src="https://www.youtube.com/embed/t-wFKNy0MZQ" 
+            src="https://youtube.com/embed/zBUe2ma_14A" 
             title="video player" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
-            </Container>
-        </Layout>    
-    );
+
+          </Container>
+          
+        </Center>
+      </CourseBanner>
+    </Layout>
+  );
 }
