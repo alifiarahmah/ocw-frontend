e77ee06e8b7844bbd3def5768e2909ee5626f67a
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-04-08 19:04:41 +0700

    feat(coursemgmt): integrate add course with useState

--- src/pages/management/course/[course-id]/quiz/add.tsx
+++ src/pages/management/course/[course-id]/quiz/add.tsx
@@ -8,12 +8,29 @@ import {
   Heading,
   HStack,
   Input,
+  useToast,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
+import { Question } from '@/types/question';
 
 export default function NewQuiz() {
   const router = useRouter();
+  const toast = useToast();
+  const [questions, setQuestions] = useState<Question[]>(
+    Array(1).fill({} as Question)
+  );
+
+  const handleSubmit = () => {
+    toast({
+      title: 'Latihan berhasil dibuat',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+    });
+    router.back();
+  };
 
   return (
     <Layout title="Latihan Baru">
@@ -24,15 +41,24 @@ export default function NewQuiz() {
         <FormLabel htmlFor="week">Week</FormLabel>
         <Input name="week" />
         <Divider my={5} />
-        <ProblemItem />
-        <Divider my={5} />
+        {questions.map((q, i) => (
+          <>
+            <ProblemItem id={i + 1} question={q} />
+            <Divider my={5} />
+          </>
+        ))}
         <HStack justifyContent="space-between">
-          <Button leftIcon={<MdAdd />} bg="biru.600" color="white">
+          <Button
+            leftIcon={<MdAdd />}
+            bg="biru.600"
+            color="white"
+            onClick={() => setQuestions([...questions, {} as Question])}
+          >
             Soal
           </Button>
           <HStack>
             <Button onClick={() => router.back()}>Batal</Button>
-            <Button bg="biru.600" color="white" onClick={() => router.back()}>
+            <Button bg="biru.600" color="white" onClick={handleSubmit}>
               Simpan
             </Button>
           </HStack>
