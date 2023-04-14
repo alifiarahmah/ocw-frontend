0c8fbaa5b171fbf314224eb95c9aaa6251671d2d
Muhammad Fahmi Irfan <13520152@std.stei.itb.ac.id>
2023-04-14 14:07:55 +0700

    feat : fix not found component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,12 @@
 import Layout from '@/components/layout'
 import { Image, Center, Text, Stack, Img } from '@chakra-ui/react'
-import Link from 'next/link'
+import NotFound from '@/components/not_found';
 
 export default function FourOhFour() {
   return (
     <>
       <Layout>
-				<Center>
-					<Stack direction={'column'}>
-						<Image src="@/asset/error.png" boxSize={100} align={'center'}/>
-						<Text fontWeight={700} fontSize={48} align={'center'}>404</Text>
-						<Text fontWeight={600} fontSize={24} align={'center'}>Page Not Found</Text>
-					</Stack>
-				</Center>
-
+				<NotFound/>
       </Layout>
     </>);
 }
