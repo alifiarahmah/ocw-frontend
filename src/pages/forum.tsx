6766ffe84271a785149f14d305d782fcd56eef7b
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-04-28 15:57:19 +0700

    feat: add coming soon to forum

--- src/pages/forum.tsx
+++ src/pages/forum.tsx
@@ -1,9 +1,10 @@
 import Layout from '@/components/layout';
+import { Heading } from '@chakra-ui/react';
 
 export default function Forum() {
   return (
     <Layout>
-      <h1>Forum</h1>
+      <Heading textAlign="center">Coming Soon</Heading>
     </Layout>
   );
 }
